71a6afab95b6d2e78a4f2574ed95eeaa2cd645b3
Pragati Brijesh Shukla <shuklap@usf.edu>
2019-11-25 03:43:31 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyAPPLICATION.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
